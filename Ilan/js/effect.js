fb22f541dd913a2cfc2fb560736d28186add6bdc
Meng-Jiun Chiou <coldmanck@gmail.com>
2016-09-14 16:54:29 +0800

    ver1

---
+++ Ilan/js/effect.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("#single_1").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+          helpers: {
+              title : {
+                  type : 'inside'
+              }
+          }
+      });
+
+    $("#single_2").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+
+    $("#single_3").fancybox({
+        openEffect  : 'elastic',
+        closeEffect : 'elastic',
+        helpers : {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
